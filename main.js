5d284d6af8193d0544d3821102481c0349af0bd1
MAREK SADOWSKI <mwsadows@us.ibm.com>
2023-10-25 08:23:42 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 async function main(params) {
-    var msg = 'The claim has been approved.';
-    if (params.name !== "") {
-        msg = 'The claim id no:  ${params.id} has been approved.'
+    var msg = `The claim has been approved.`;
+    if (params.id) {
+        msg = `The claim id no:  ${params.id} has been approved.`
     }
     return {
         headers: { 'Content-Type': 'text/plain;charset=utf-8' },
